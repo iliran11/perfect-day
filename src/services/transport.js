0b0f594df735eeac8599020ea7696e676dbfeddc
Liran Cohen <iliran11@gmail.com>
2020-07-31 12:39:27 +0300

    send value to server

--- src/services/transport.js
+++ src/services/transport.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import getConfig from "next/config";
 
-const {publicRuntimeConfig} = getConfig()
+const { publicRuntimeConfig } = getConfig();
 
 export const sendDay = (day) => {
   axios({
     method: "post",
-    data: day,
+    data: {
+      day,
+    },
     url: `${publicRuntimeConfig.serverUrl}/day`,
   });
 };
